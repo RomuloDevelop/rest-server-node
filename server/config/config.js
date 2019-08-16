3e70ff377ba9005e53f3b615d1df4a7592039971
romulo <romulocg25@gmail.com>
2019-08-16 11:21:09 -0400

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else 
-    urlDB = 'mongodb+srv://romulo:jNvh5qiHJthu1LFo@cluster0-kfpnd.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
 
